37afafa1fb4ee79223bd963973b7d2d74eb352d4
Denis T. <fallenmail@yandex.ru>
2024-04-02 13:41:36 +0300

    step_6

--- qa_guru_python_7_6-main/homework.py
+++ qa_guru_python_7_6-main/homework.py
@@ -7,12 +7,28 @@ def test_dark_theme_by_time():
     """
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-
-    is_dark_theme = None
+    if time(22) < current_time or current_time < time(6, 0, 1):
+        is_dark_theme = True
+    else:
+        is_dark_theme = False
     assert is_dark_theme is True
 
 
 def test_dark_theme_by_time_and_user_choice():
+
+    current_time = time(hour=16)
+    dark_theme_enabled_by_user = True
+
+    if dark_theme_enabled_by_user is None:
+        if time(22) < current_time or current_time < time(6, 0, 1):
+            is_dark_theme = True
+        else:
+            is_dark_theme = False
+
+    else:
+        is_dark_theme = dark_theme_enabled_by_user
+    assert is_dark_theme is True
+
     """
     Протестируйте правильность переключения темной темы на сайте
     в зависимости от времени и выбора пользователя
@@ -20,13 +36,9 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = False - Темная тема выключена
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
-    current_time = time(hour=16)
-    dark_theme_enabled_by_user = True
+
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
-
-    is_dark_theme = None
-    assert is_dark_theme is True
 
 
 def test_find_suitable_user():
@@ -43,15 +55,22 @@ def test_find_suitable_user():
 
     # TODO найдите пользователя с именем "Olga"
     suitable_users = None
+    for i in users:
+        if i["name"] == "Olga":
+            suitable_users = i
+            break
     assert suitable_users == {"name": "Olga", "age": 45}
 
     # TODO найдите всех пользователей младше 20 лет
-    suitable_users = None
+    suitable_users = []
+    for i in users:
+        if i["age"] < 20:
+            suitable_users.append(i)
+
     assert suitable_users == [
         {"name": "Stanislav", "age": 15},
         {"name": "Maria", "age": 18},
     ]
-
 
 # Сделайте функцию, которая будет печатать
 # читаемое имя переданной ей функции и значений аргументов.
@@ -67,19 +86,28 @@ def test_find_suitable_user():
 def test_readable_function():
     open_browser(browser_name="Chrome")
     go_to_companyname_homepage(page_url="https://companyname.com")
-    find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
+    find_registration_button_on_login_page(
+        page_url="https://companyname.com/login", button_text="Register")
 
 
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = print_func_and_args(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = print_func_and_args(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = print_func_and_args(
+        find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+
+
+def print_func_and_args(func, *args):
+    func_name = func.__name__.replace('_', ' ').title()
+    args_name = ", ".join([*args])
+    print(f"{func_name} [{args_name}]")
+    return f"{func_name} [{args_name}]"
